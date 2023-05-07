d1ecdab2f55420358dd2c9ceef8d7fee4ccf05f7
JazlynL <lubenjazlyn35@gmail.com>
2023-05-07 12:39:15 -0700

    first c sharp init

---
+++ src/C.java
@@ -0,0 +1,15 @@
+public class C {
+
+    //
+    public class Program
+    {
+        public static bool lessThan100(int a, int b)
+        {
+            if(a+b < 100){
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
